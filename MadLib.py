5c2a5afdaf3cf3f8526804b02bb34e92add9f4a3
baggedmilk999 <89410031+baggedmilk999@users.noreply.github.com>
2022-08-24 07:52:28 -0400

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,19 @@
+#Add User Input
+#Think of funi idea
+print("GIVE ME A NOUN")
+
+noun = input("Type Noun:\n")
+
+print("I ate dinner with a " + noun)
+
+print("GIVE ME AN ADJECTIVE")
+
+adjective = input("Type Adjective:\n")
+
+print("They were very " + adjective)
+
+print("GIVE ME A VERB")
+
+verb = input("Type Verb\n")
+
+print("We " + verb + " together")
